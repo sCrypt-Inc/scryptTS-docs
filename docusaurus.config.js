0e61b03355894b10f9682bda83fee3e64cb3717a
hh <hh@scrypt.io>
2022-11-23 11:04:12 +0800

    fix organizationName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'sCrypt', // Usually your GitHub org/user name.
+  organizationName: 'sCrypt-Inc', // Usually your GitHub org/user name.
   projectName: 'scrypt-ts', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
