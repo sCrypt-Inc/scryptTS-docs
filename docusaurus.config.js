1cba2926cd66a15175161c7167915ba4f6cf1e1a
hh <hh@scrypt.io>
2022-12-04 09:20:02 +0800

    change projectName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sCrypt-Inc', // Usually your GitHub org/user name.
-  projectName: 'scryptTS', // Usually your repo name.
+  projectName: 'scrypt-ts', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
