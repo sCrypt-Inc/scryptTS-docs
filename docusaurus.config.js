2eb30e3c6d52617f3f46cba7909e75f2c41d891c
Mihael Šinkec <sinkec.mihael@gmail.com>
2023-03-19 18:37:45 +0100

    Logo update.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -51,10 +51,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'sCrypt',
+        title: '',
         logo: {
           alt: 'scrypt Logo',
-          src: 'img/logo.svg',
+          src: 'img/scrypt.png',
         },
         items: [
           {
