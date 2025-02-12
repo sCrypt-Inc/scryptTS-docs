8865d91d1a7b05f589320592495c9b054b115550
Ryan Wold <wold@afomi.com>
2025-02-11 19:29:56 -0800

    update to docusaurus 3
    *

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -82,8 +83,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} sCrypt, Inc.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
 
       algolia: {
@@ -102,7 +103,6 @@ const config = {
         path: 'open-api',
         routeBasePath: 'open-api',
         sidebarPath: './open-api/sidebars.js',
-        docLayoutComponent: "@theme/DocPage",
         docItemComponent: "@theme/ApiItem" // derived from docusaurus-theme-openapi-docs
       },
     ],
